25f48b11a2931f226704fa7caa06cc9f3f13f5b3
taiga <taigasano4112@gmail.com>
2015-09-22 20:11:03 +0900

    podfileのflatformを8.1に変更

---
+++ iot-project/iot-project/PlayMapView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayMapView.swift
+//  iot-project
+//
+//  Created by wtnv009 on 2015/09/22.
+//  Copyright © 2015年 Shinnosuke Komiya. All rights reserved.
+//
+
+import UIKit
+
+class PlayMapView: GMSMapView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
